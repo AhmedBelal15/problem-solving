3aeff6cac6d01baa00fc42110d9b0e32ea8ab558
ahmedbelal15 <ahmbelal95@gmail.com>
2021-02-21 14:09:49 +0200

    subrectangle queries

---
+++ 1476 Subrectangle Queries/answer.js
@@ -0,0 +1,18 @@
+class SubrectangleQueries {
+    constructor(rectangle) {
+        this.rectangle = [...rectangle]
+        return this.rectnagle
+    }
+
+    updateSubrectangle(row1, col1, row2, col2, newValue) {
+        for(let i = row1; i <= row2; i++){
+            for(let j = col1; j <= col2; j++){
+                this.rectangle[i][j] = newValue
+            }
+        }
+    }
+
+    getValue(row, col) {
+        return this.rectangle[row][col]
+    }
+}
